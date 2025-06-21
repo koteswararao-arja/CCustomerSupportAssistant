db800d0504259885a13d782aa04a31896939c5c8
Koteswararao <arja.koteswararao@email.com>
2025-06-21 11:46:07 +0530

    Required UI files added

---
+++ customer-support-ui/src/app/features/ticket/ticket.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.css']
+})
+export class TicketComponent {}
